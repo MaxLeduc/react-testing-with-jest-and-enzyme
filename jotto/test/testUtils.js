9c5996102a09ae114802d6663fa18487ca261e07
Mona Lisa <leduc.maximep@gmail.com>
2018-12-04 14:19:54 -0500

    prop types tests

--- jotto/test/testUtils.js
+++ jotto/test/testUtils.js
@@ -1,3 +1,5 @@
+import checkPropTypes from 'check-prop-types';
+
 /**
   * Return ShallowWrapper container node(s) with the given data-test value.
   * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper to search within.
@@ -7,4 +9,22 @@
 
 export const findByTestAttr = (wrapper, val) => {
   return wrapper.find(`[data-test='${val}']`)
+}
+
+/**
+  * Return check value depending on if the proptypes are correct.
+  * @param {ShallowWrapper} component - Enzyme shallow wrapper to test props.
+  * @param {object} conformingProps - props to test.
+  * @returns test
+*/
+
+export const checkProps = (component, conformingProps) => {
+  const propError = checkPropTypes(
+    component.propTypes,
+    conformingProps,
+    'prop',
+    component.name
+  );
+
+  expect(propError).toBeUndefined();
 }
