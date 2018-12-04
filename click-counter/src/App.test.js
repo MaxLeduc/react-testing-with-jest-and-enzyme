0cb8980a9948bfba9825818ff727b52b04e329d1
Mona Lisa <leduc.maximep@gmail.com>
2018-12-04 10:29:12 -0500

    Initial Commit

---
+++ click-counter/src/App.test.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+import App from './App';
+
+Enzyme.configure({ adapter: new EnzymeAdapter()});
+
+/**
+  * Factory function to create a ShallowWrapper for the App component.
+  * @function setup
+  * @param {object} props - Component props specific to this setup.
+  * @param {any} state - Initial state for setup.
+  * @returns {ShallowWrapper}
+*/
+
+const setup = (props = {}, state = {}) => {
+  return shallow(<App {...props} />)
+}
+
+/**
+  * Return ShallowWrapper container node(s) with the given data-test value.
+  * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper to search within.
+  * @param {string} val - Value of data-test attribute for search.
+  * @returns {ShallowWrapper}
+*/
+
+const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test='${val}']`)
+}
+
+test('renders without an error', () => {
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper, 'component-app');
+  expect(appComponent.length).toBe(1);
+})
+
+test('renders increment button', () => {
+  const wrapper = setup();
+  const button = findByTestAttr(wrapper, 'increment-button');
+  expect(button.length).toBe(1);
+})
+
+test('renders counter display', () => {
+  const wrapper = setup();
+  const counterDisplay = findByTestAttr(wrapper, 'counter-display');
+  expect(counterDisplay.length).toBe(1);
+})
+
+test('counter starts at 0', () => {
+
+})
+
+test('clicking button increments counter display', () => {
+
+})
---
+++ demo/src/App.test.js
@@ -0,0 +1,13 @@
+/* global it, expect */
+
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16'
+import App from './App';
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+it('renders without crashing', () => {
+  const wrapper = shallow(<App />)
+  expect(wrapper).toBeTruthy();
+});
