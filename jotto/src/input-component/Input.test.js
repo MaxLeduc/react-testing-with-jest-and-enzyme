e883b57a0ed7057e2ef29723f55bdf9ebda4fb51
Mona Lisa <leduc.maximep@gmail.com>
2018-12-05 11:59:30 -0500

    added markup to input component

--- jotto/src/input-component/Input.test.js
+++ jotto/src/input-component/Input.test.js
@@ -13,20 +13,30 @@ import Input from './Input';
 const setup = (initialState = {}) => {
   const store = storeFactory();
   const wrapper = shallow(<Input store={store} />).dive();
+  return wrapper;
 }
 
 describe('render', () => {
   describe('word has not been guessed', () => {
-    test('renders component without error', () => {
+    let wrapper;
+    beforeEach(() => {
+      const initialState = { success: false };
+      wrapper = setup(initialState);
+    })
 
+    test('renders component without error', () => {
+      const component = findByTestAttr(wrapper, 'component-input');
+      expect(component.length).toBe(1);
     });
 
     test('renders input box', () => {
-
+      const component = findByTestAttr(wrapper, 'input-box');
+      expect(component.length).toBe(1);
     });
 
     test('renders submit button', () => {
-
+      const component = findByTestAttr(wrapper, 'submit-button');
+      expect(component.length).toBe(1);
     })
   });
 
