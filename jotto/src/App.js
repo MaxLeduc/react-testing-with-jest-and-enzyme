f3dff5f4028d9a1d2bd48e3b3213e633467cc1e3
Mona Lisa <leduc.maximep@gmail.com>
2018-12-06 16:12:25 -0500

    added props and tests to App

--- jotto/src/App.js
+++ jotto/src/App.js
@@ -1,21 +1,45 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 import './App.css';
 
+import { guessWord } from './actions';
 import GuessedWords from './guessedWords-component/GuessedWords';
-import Congrats from './congrats-component/Congrats.js';
+import Congrats from './congrats-component/Congrats';
+import Input from './input-component/Input';
 
 class App extends Component {
   render() {
+    const {
+      secretWord,
+      success,
+      guessedWords,
+      getSecretWord,
+    } = this.props;
+
     return (
-      <div className="container">
+      <div className="container" data-test="app-component">
         <h1>Jotto</h1>
-        <Congrats success={true} />
-        <GuessedWords guessedWords={[
-          { guessedWord: 'train', letterMatchCount: 3}
-        ]} />
+        <Congrats success={success} />
+        <Input />
+        <GuessedWords guessedWords={guessedWords} />
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = ({success, guessedWords, secretWord}) => {
+  return {
+    success,
+    guessedWords,
+    secretWord,
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getSecretWord: bindActionCreators(guessWord, dispatch),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- jotto/src/App.test.js
+++ jotto/src/App.test.js
@@ -1,9 +1,52 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+
+import { storeFactory, findByTestAttr } from '../test/testUtils'
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+/**
+  * Factory function to create a ShallowWrapper for the Input component.
+  * @function setup
+  * @param {object} initialState - Initial state for this setup.
+  * @returns {ShallowWrapper}
+*/
+const setup = (initialState = {}) => {
+  const store = storeFactory(initialState);
+  const wrapper = shallow(<App store={store} />).dive();
+  return wrapper;
+}
+
+describe('renders without error', () => {
+  const wrapper = setup();
+  const AppComponent = findByTestAttr(wrapper, 'app-component')
+  expect(AppComponent.length).toBe(1);
+})
+
+describe('redux props', () => {
+  test('has `success` as a piece of state', () => {
+    const success = true
+    const wrapper = setup({ success });
+    const successProp = wrapper.instance().props.success;
+    expect(successProp).toBe(success);
+  })
+
+  test('has `guessedWords` as a piece of state', () => {
+    const guessedWords = [{guessedWord: 'train', letterMatchCount: 3}];
+    const wrapper = setup({ guessedWords });
+    const guessedWordsProp = wrapper.instance().props.guessedWords;
+    expect(guessedWordsProp).toBe(guessedWords);
+  })
+
+  test('has `secretWord` as a piece of state', () => {
+    const secretWord = 'party';
+    const wrapper = setup({ secretWord });
+    const secretWordProp = wrapper.instance().props.secretWord;
+    expect(secretWordProp).toBe(secretWord);
+  })
+
+  test('`getSecretWord` action creator is a function prop', () => {
+    const wrapper = setup();
+    const getSecretWordProp = wrapper.instance().props.getSecretWord;
+    expect(getSecretWordProp).toBeInstanceOf(Function);
+  })
+})
